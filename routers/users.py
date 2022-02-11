afdb497da66aeec7f4f11be60416dc8b84d53a44
Ravi Mashru <mashru.ravi@gmail.com>
2022-02-11 16:08:48 +0530

    add route to get user details

--- routers/users.py
+++ routers/users.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_user():
     return "New User Created"
+
+@router.get("/{id}")
+async def get_user_details(id: int):
+    return f"Details for user {id}"
