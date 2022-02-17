e974a18e5abaeac1258600004919bc40603160ee
Kurian Benoy <kurian.bkk@gmail.com>
2022-02-17 23:56:46 +0530

    Update test_checkpoint.py

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -4,4 +4,4 @@ from app.test_main import client
 def test_checkpoint():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json == "The server is alive..."
+    assert response.json() == "The server is alive..."
