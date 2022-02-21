5ffa1d0d7a7e836f7ea6d4b83f3ff34d6fa34dad
Ravi Mashru <mashru.ravi@gmail.com>
2022-02-21 23:12:33 +0530

    set up project for tests

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+# See: https://docs.pytest.org/en/latest/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
+
+
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+from httpx import AsyncClient
+
+from app.main import app
+
+@pytest.mark.asyncio
+async def test_root():
+  async with AsyncClient(app=app, base_url="http://test") as ac:
+    response = await ac.get("/")
+  assert response.status_code == 200
