1d8af064f64d2a32f37bfe43ea2f9b4411018c1b
Kurian Benoy <kurian.bkk@gmail.com>
2022-02-21 22:27:30 +0530

    Update inference.py

--- app/inference.py
+++ app/inference.py
@@ -1,5 +1,12 @@
-# from fastai
+from fastai.collab import *
+from fastai.tabular.all import *
+from torch import nn
 # from app.models.user_movies import Use
 
-def get_recommendations():
-  pass
+learn = load_learner("ml_model/movie-recommender-nn.pkl")
+
+def get_recommendations(user_id):
+    # corresponding to userid get rating
+    
+    #calclulate cosine similarity and return top K predictions
+    pass
