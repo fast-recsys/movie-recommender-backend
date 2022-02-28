1ec43c9236fd0097c38f8ef3fd7c26b504808453
Ravi Mashru <mashru.ravi@gmail.com>
2022-02-28 20:49:55 +0530

    refactor movie tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,9 @@ from typing import Any
 import httpx
 import pytest
 from asgi_lifespan import LifespanManager
-import pandas as pd
 
 # Override configuration variables for tests
 import os
-from app.config import Settings
-from app.data import get_movie_details_from_tmdb, get_movie_df, get_tmdb_id
-from app.models.movie import MoviePublic
 
 os.environ["MONGODB_URL"] = "mongodb://localhost:27017"
 os.environ["MONGODB_DBNAME"] = "movie-rec"
@@ -27,13 +23,6 @@ def event_loop():
   loop = asyncio.get_event_loop()
   yield loop
   loop.close()
-
-def mock_get_movie_df() -> Any:
-  mock_data = { 'movieId': [1], 'imdbId': [321], 'tmdbId': [123] }
-  return pd.DataFrame(mock_data)
-
-def mock_get_movie_details_from_tmdb() -> MoviePublic:
-  return MoviePublic(id=123, title="Mock Title", thumbnail_url="https://example.com/thumbnail.png")
 
 @pytest.fixture(scope="module")
 async def test_client():
--- tests/test_movies.py
+++ tests/test_movies.py
@@ -5,10 +5,19 @@
 from app.data import get_movie_details_from_tmdb, get_movie_df
 from app.main import app
 
+from app.data import get_movie_details_from_tmdb, get_movie_df
+from app.models.movie import MoviePublic
+
 from httpx import AsyncClient, codes
 import pytest
+import pandas as pd
 
-from tests.conftest import mock_get_movie_details_from_tmdb, mock_get_movie_df
+def mock_get_movie_df():
+  mock_data = { 'movieId': [1], 'imdbId': [321], 'tmdbId': [123] }
+  return pd.DataFrame(mock_data)
+
+def mock_get_movie_details_from_tmdb() -> MoviePublic:
+  return MoviePublic(id=123, title="Mock Title", thumbnail_url="https://example.com/thumbnail.png")
 
 @pytest.mark.asyncio
 async def test_get_known_movie_details(test_client: AsyncClient):
